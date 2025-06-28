e8a2f1c27c6937c4c47325d909b4f403a044b2e4
Kimgyuilli <rlarbdlf222@gmail.com>
2025-06-28 23:20:13 +0900

    init: initial commit

---
+++ src/main/java/com/_ithon/speeksee/SpeekseeApplication.java
@@ -0,0 +1,13 @@
+package com._ithon.speeksee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeekseeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeekseeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_ithon/speeksee/SpeekseeApplicationTests.java
@@ -0,0 +1,13 @@
+package com._ithon.speeksee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeekseeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
