71af0d8d294befcccde447cfc2ed4b97306392dd
Jiwon-Yoon00 <0yuniverse0@gmail.com>
2025-06-30 21:42:42 +0900

    feat member: 회원가입을 위한 Member 엔티티, 레포지토리, DTO 추가

---
+++ src/main/java/com/_ithon/speeksee/domain/member/entity/Member.java
@@ -0,0 +1,65 @@
+package com._ithon.speeksee.domain.member.entity;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Member {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id")
+	private Long id;
+
+	@Column(nullable = false, unique = true)
+	private String username;
+
+	@Column(nullable = false, unique = true)
+	private String email;
+
+	@Column(nullable = false)
+	private String passwordHash;
+
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
+
+	@Column(nullable = false)
+	private String currentLevel;
+
+	@Column(nullable = false)
+	private Integer totalExp;
+
+	private LocalDate lastLogin;
+
+	@Column(nullable = false)
+	private Integer consecutiveDays;
+
+	@PrePersist // 엔티티를 처음 저장할 때
+	protected void onCreate() {
+		createdAt = updatedAt = LocalDateTime.now();
+	}
+
+	@PreUpdate // 엔티티 수정 시
+	protected void onUpdate() {
+		updatedAt = LocalDateTime.now();
+	}
+}
---
+++ src/main/java/com/_ithon/speeksee/domain/member/repository/MemberRepository.java
@@ -0,0 +1,16 @@
+package com._ithon.speeksee.domain.member.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com._ithon.speeksee.domain.member.entity.Member;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+	Optional<Member> findByMemberEmail(String email);
+
+	// 이메일 중복 확인
+	boolean existsByEmail(String email);
+}
