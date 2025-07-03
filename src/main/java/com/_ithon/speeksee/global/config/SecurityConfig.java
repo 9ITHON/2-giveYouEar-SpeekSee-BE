a427010af5634964881edae4974546e1ab569576
Jiwon-Yoon00 <0yuniverse0@gmail.com>
2025-07-03 14:04:40 +0900

    feat auth: SecurityConfig에 JWT 인증 필터 등록

--- src/main/java/com/_ithon/speeksee/global/config/SecurityConfig.java
+++ src/main/java/com/_ithon/speeksee/global/config/SecurityConfig.java
@@ -13,14 +13,18 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 
 import java.util.List;
+
+import com._ithon.speeksee.global.auth.jwt.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
 
 //    private final JWTUtil jwtUtil;
@@ -76,6 +80,7 @@ public class SecurityConfig {
                         .authenticationEntryPoint((request, response, authException) ->
                                 response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized")));
 
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
 //        http.oauth2Login(oauth2 -> oauth2
 //                .successHandler(oAuth2LoginSuccessHandler)
