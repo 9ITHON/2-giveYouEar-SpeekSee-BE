270ebbba7f88e38ea63acbf8033bc1b78788091a
Jiwon-Yoon00 <0yuniverse0@gmail.com>
2025-07-03 09:57:25 +0900

    refactor auth: 생성자 관련 아노테이션 추가

--- src/main/java/com/_ithon/speeksee/global/auth/entity/RefreshToken.java
+++ src/main/java/com/_ithon/speeksee/global/auth/entity/RefreshToken.java
@@ -12,15 +12,20 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Builder
 @Getter
 @Setter
-public class RefreshToken{
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class RefreshToken {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
