16d1a974a8d6009bee376ad7b4231cf4b14b7a62
Jiwon-Yoon00 <0yuniverse0@gmail.com>
2025-07-05 22:11:08 +0900

    docs attendance: 스웨거 문서 작성

--- src/main/java/com/_ithon/speeksee/domain/attendance/controller/AttendanceController.java
+++ src/main/java/com/_ithon/speeksee/domain/attendance/controller/AttendanceController.java
@@ -14,6 +14,12 @@ import com._ithon.speeksee.domain.member.entity.Member;
 import com._ithon.speeksee.global.auth.model.CustomUserDetails;
 import com._ithon.speeksee.global.infra.exception.response.ApiRes;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -22,6 +28,36 @@ import lombok.RequiredArgsConstructor;
 public class AttendanceController {
 	private final AttendanceService attendanceService;
 
+	@Operation(
+		summary = "출석 캘린더 조회",
+		description = "사용자의 연도 및 월별 출석 현황을 조회합니다."
+	)
+	@ApiResponses(value = {
+		@ApiResponse(
+			responseCode = "200",
+			description = "출석체크 조회 성공",
+			content = @Content(
+				mediaType = "application/json",
+				schema = @Schema(implementation = AttendanceStatusResponseDto.class),
+				examples = @ExampleObject(
+					name = "성공 예시",
+					value = """
+						{
+						  "status": 200,
+						  "message": "출석체크 조회",
+						  "data": {
+						    "year": 2025,
+						    "month": 7,
+						    "attendanceDates": [
+						      "2025-07-05"
+						    ]
+						  }
+						}
+						"""
+				)
+			)
+		)
+	})
 	@GetMapping("/calendar")
 	public ResponseEntity<ApiRes<AttendanceStatusResponseDto>> getAttendanceCalendar(
 		@RequestParam int year,
