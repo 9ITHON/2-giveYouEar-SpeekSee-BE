3774052bf5a979f6d8c77ca9abcd8773b9d03c55
Jiwon-Yoon00 <0yuniverse0@gmail.com>
2025-07-08 07:11:11 +0900

    docs: 스웨거 문서 정리

--- src/main/java/com/_ithon/speeksee/global/auth/dto/request/OAuthRequestDto.java
+++ src/main/java/com/_ithon/speeksee/global/auth/dto/request/OAuthRequestDto.java
@@ -2,10 +2,16 @@ package com._ithon.speeksee.global.auth.dto.request;
 
 import com._ithon.speeksee.domain.member.entity.AuthProvider;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
+@Schema(description = "소셜 로그인 요청 DTO")
 public class OAuthRequestDto {
+
+	@Schema(description = "소셜 인가 코드", example = "abc123code...")
 	private String code;
+
+	@Schema(description = "소셜 로그인 공급자", example = "KAKAO")
 	private AuthProvider provider;
 }
