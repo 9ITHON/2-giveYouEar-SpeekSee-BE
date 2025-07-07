e8ed15b9bcab9e874bb603898ebb5879379e6774
Jiwon-Yoon00 <0yuniverse0@gmail.com>
2025-07-07 23:35:12 +0900

    refactor member, auth: - username->nickname 수정 - birthday 추가 - DTO 및 정적 메서드(from/toEntity) 수정 - @Builder와 중복되는 @AllArgsConstructor 제거

--- src/main/java/com/_ithon/speeksee/domain/member/dto/response/MemberInfoResponseDto.java
+++ src/main/java/com/_ithon/speeksee/domain/member/dto/response/MemberInfoResponseDto.java
@@ -1,5 +1,6 @@
 package com._ithon.speeksee.domain.member.dto.response;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import com._ithon.speeksee.domain.member.entity.Member;
@@ -11,7 +12,6 @@ import lombok.Getter;
 
 @Getter
 @Builder
-@AllArgsConstructor
 @Schema(description = "사용자 정보 DTO (로그인 응답 및 내 정보 조회 공통 사용)")
 public class MemberInfoResponseDto {
 
@@ -21,8 +21,11 @@ public class MemberInfoResponseDto {
 	@Schema(description = "이메일", example = "user@example.com")
 	private final String email;
 
-	@Schema(description = "이름 또는 닉네임", example = "홍길동")
-	private final String username;
+	@Schema(description = "닉네임", example = "길동길동")
+	private final String nickname;
+
+	@Schema(description = "생년월일", example = "2025-07-07")
+	private final LocalDate birthday;
 
 	@Schema(description = "현재 레벨", example = "초급")
 	private final String currentLevel;
@@ -36,14 +39,17 @@ public class MemberInfoResponseDto {
 	@Schema(description = "계정 생성일시", example = "2025-06-20T10:00:00Z", required = false)
 	private final LocalDateTime createdAt;  // 로그인 시점에는 null이어도 됨
 
-	public MemberInfoResponseDto(Member member) {
-		this.userId = member.getId();
-		this.email = member.getEmail();
-		this.username = member.getUsername();
-		this.currentLevel = member.getCurrentLevel();
-		this.totalExp = member.getTotalExp();
-		this.consecutiveDays = member.getConsecutiveDays();
-		this.createdAt = member.getCreatedAt();
+	public static MemberInfoResponseDto from(Member member) {
+		return MemberInfoResponseDto.builder()
+			.userId(member.getId())
+			.email(member.getEmail())
+			.nickname(member.getNickname())
+			.birthday(member.getBirthday())
+			.currentLevel(member.getCurrentLevel())
+			.totalExp(member.getTotalExp())
+			.consecutiveDays(member.getConsecutiveDays())
+			.createdAt(member.getCreatedAt())
+			.build();
 	}
 
 }
--- src/main/java/com/_ithon/speeksee/global/auth/dto/response/GoogleUserInfoResponseDto.java
+++ src/main/java/com/_ithon/speeksee/global/auth/dto/response/GoogleUserInfoResponseDto.java
@@ -3,24 +3,26 @@ package com._ithon.speeksee.global.auth.dto.response;
 import java.util.Map;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
 @AllArgsConstructor
-public class GoogleUserInfoResponseDto implements OAuth2UserInfo{
+public class GoogleUserInfoResponseDto implements OAuth2UserInfo {
 
 	private Map<String, Object> attributes;
 
 	@Override
-	public String getEmail() {
-		return (String) attributes.get("email");
+	public final String getEmail() {
+		return (String)attributes.get("email");
 	}
 
 	@Override
-	public String getName() {
-		return (String) attributes.get("name");
+	public final String getName() {
+		return (String)attributes.get("name");
 	}
 
 	@Override
-	public Map<String, Object> getAttributes() {
+	public final Map<String, Object> getAttributes() {
 		return attributes;
 	}
 }
