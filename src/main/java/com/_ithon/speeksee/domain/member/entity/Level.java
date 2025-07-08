1e8a363d7e38c49b7a88179d8b67976aef0f1e27
Jiwon-Yoon00 <0yuniverse0@gmail.com>
2025-07-09 01:31:24 +0900

    feat member: 정확도 기반 사용자 레벨 결정 로직

--- src/main/java/com/_ithon/speeksee/domain/member/entity/Level.java
+++ src/main/java/com/_ithon/speeksee/domain/member/entity/Level.java
@@ -3,5 +3,14 @@ package com._ithon.speeksee.domain.member.entity;
 public enum Level {
 	Beginner,
 	Intermediate,
-	Advanced
+	Advanced;
+
+	public static Level determineLevel(double accuracy) {
+		double score = accuracy * 100;
+		if (score >= 90.0)
+			return Advanced;
+		if (score >= 80.0)
+			return Intermediate;
+		return Beginner;
+	}
 }
