c68744cea219c7fae17379aff7f89b7622e113fd
Jiwon-Yoon00 <0yuniverse0@gmail.com>
2025-07-10 14:31:02 +0900

    feat script: 저장된 대본 5개 중 랜덤으로 3개 가져오기

--- src/main/java/com/_ithon/speeksee/domain/script/repository/ScriptRepository.java
+++ src/main/java/com/_ithon/speeksee/domain/script/repository/ScriptRepository.java
@@ -9,8 +9,8 @@ import com._ithon.speeksee.domain.script.domain.Script;
 
 public interface ScriptRepository extends JpaRepository<Script, Long>, ScriptRepositoryCustom {
 
-	@Query("SELECT s FROM Script s WHERE s.isLevelTest = true")
-	List<Script> findAllLevelTestScripts();
+	@Query(value = "SELECT * FROM script WHERE is_level_test = true ORDER BY RANDOM() LIMIT 3", nativeQuery = true)
+	List<Script> find3ByIsLevelTestTrue();
 
 	boolean existsByIsLevelTest(boolean isLevelTest);
 }
